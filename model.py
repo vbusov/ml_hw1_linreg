63555bc6574febc1f57dd43035766b50aefe92b0
Vlad Busov <busov.dev@gmail.com>
2023-12-05 21:06:52 +0300

    Update files

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pickle
 
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -7,10 +8,7 @@ from sklearn.compose import ColumnTransformer
 
 
 OHE = "pickles/preprocessor.pickle"
-MODELS = {
-    "ridge": "pickles/ridge.pickle",
-}
-USE_MODEL = "ridge"
+MODEL = "pickles/model.pickle"
 
 
 def load_object(filename: str): 
@@ -20,18 +18,49 @@ def load_object(filename: str):
 
 def preprocess(input_objects):
     df = pd.DataFrame(input_objects)
+
     for column in ["engine", "mileage", "max_power"]:
         num_regex = r'(\d*\.\d+|\d+)'
         df[column] = df[column].str.extract(num_regex, expand=False).astype(float)
+    df.drop(["torque"], axis=1, inplace=True)
+
     for column in ["seats", "engine"]:
         df[column] = df[column].astype(int)
-    df.drop("torque", axis=1, inplace=True)
+
+    # extract maker and model info from car name
+    df["maker"] = df["name"].str.split(expand=True)[0]
+    df["model"] = df["name"].str.split(expand=True)[1]
+    df["model"] = df["model"].str.lower()
+
+    # construct car age and take log
+    df["car_age"] = df["year"].max() - df["year"]
+    df["car_age_log"] = np.log(df["car_age"] + 1)
+
+    # take log of km_driven
+    df["km_driven_log"] = np.log(df["km_driven"] + 1)
+    df["mileage_log"] = np.log(df["mileage"] + 1)
+
+    # combine features
+    df["max_power_x_engine"] = df["max_power"] * df["engine"]
+    df["max_power_x_engine_log"] = np.log(df["max_power_x_engine"] + 1)
+
+    df["car_age_log_x_km_driven_log"] = df["car_age_log"] * df["km_driven_log"]
+    df["car_age_log_x_km_driven_log_log"] = np.log(df["car_age_log_x_km_driven_log"] + 1)
+
+    df["car_age_log_x_mileage_log"] = df["car_age_log"] * df["mileage_log"]
+    df["car_age_log_x_mileage_log_log"] = np.log(df["car_age_log_x_mileage_log"] + 1)
     
     ohe = load_object(OHE)
     columns = ohe.get_feature_names_out()
-    return pd.DataFrame(ohe.transform(df), columns=columns)
+
+    df = ohe.transform(df)
+    if ohe.sparse_output_:
+      df = df.toarray()
+    df = pd.DataFrame(df, columns=columns)
+
+    return df
 
 
-def predict(data, use_model=USE_MODEL):
-    predictor = load_object(MODELS[use_model])
+def predict(data, use_model=MODEL):
+    predictor = load_object(MODEL)
     return predictor.predict(data)
